77156cf44297591fb7577292f01df932e9dd8917
yanlele <331393627@qq.com>
2021-01-24 21:48:32 +0800

    feat: 配置文件

--- packages/client/config/index.go
+++ packages/client/config/index.go
@@ -1,3 +1,69 @@
 package config
 
+import (
+	"github.com/go-ini/ini"
+	"log"
+	"time"
+)
 
+type App struct {
+	JwtSecret       string
+	PageSize        int
+	RuntimeRootPath string
+	ImagePrefixUrl  string
+	ImageSavePath   string
+	ImageMaxSize    int
+	ImageAllowExts  []string
+	LogSavePath     string
+	LogSaveName     string
+	LogFileExt      string
+	TimeFormat      string
+}
+
+var AppConfig = &App{}
+
+type Server struct {
+	RunMode      string
+	HttpPort     int
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+var ServerConfig = &Server{}
+
+type Database struct {
+	Type        string
+	User        string
+	Password    string
+	Host        string
+	Name        string
+	TablePrefix string
+}
+
+var DatabaseConfig = &Database{}
+
+func SetUp() {
+	appConfig, err := ini.Load("config/app.ini")
+	if err != nil {
+		log.Fatal("初始化配置文件失败: ", err)
+	}
+	err = appConfig.Section("app").MapTo(AppConfig)
+	if err != nil {
+		log.Fatal("config mapTo appConfig err: ", err)
+	}
+
+	AppConfig.ImageMaxSize = AppConfig.ImageMaxSize * 1024 * 1024
+
+	err = appConfig.Section("server").MapTo(ServerConfig)
+	if err != nil {
+		log.Fatalf("config mapTo ServerSetting err: %v", err)
+	}
+
+	ServerConfig.ReadTimeout = ServerConfig.ReadTimeout * time.Second
+	ServerConfig.WriteTimeout = ServerConfig.WriteTimeout * time.Second
+
+	err = appConfig.Section("database").MapTo(DatabaseConfig)
+	if err != nil {
+		log.Fatalf("config mapTo DatabaseSetting err: %v", err)
+	}
+}
