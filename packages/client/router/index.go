979461fb6ecc7b5edc2776ffc07c6bffd7c0d67b
yanlele <331393627@qq.com>
2021-01-29 20:54:01 +0800

    feat: 添加controller

--- packages/client/router/index.go
+++ packages/client/router/index.go
@@ -5,9 +5,11 @@ import "github.com/gin-gonic/gin"
 func SetUpRouter() *gin.Engine {
 	router := gin.Default()
 
-
 	index := router.Group("/api")
 	{
-
+		userRouter := index.Group("/user")
+		{
+			userRouter.GET("/:name")
+		}
 	}
 }
