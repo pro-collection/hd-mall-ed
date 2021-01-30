838af008d7185b622667cf479229ff58e37ef83e
yanlele <331393627@qq.com>
2021-01-30 20:02:07 +0800

    feat: 创建用户

--- packages/client/controller/userController/index.go
+++ packages/client/controller/userController/index.go
@@ -1,3 +1,21 @@
 package userController
 
+import (
+	"github.com/gin-gonic/gin"
+	. "hd-mall-ed/packages/client/models/userModel"
+	"net/http"
+)
 
+// 创建用户信息
+func CreateUser(c *gin.Context) {
+	var err error
+	user := &User{}
+
+	err = c.ShouldBindJSON(user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    -1,
+			"message": "参数错误",
+		})
+	}
+}
