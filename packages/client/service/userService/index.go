a05ed9cdd9b20c4d4d8725156b55043958a4e32d
yanlele <331393627@qq.com>
2021-02-05 00:14:46 +0800

    feat: 更新相关逻辑

--- packages/client/service/userService/index.go
+++ packages/client/service/userService/index.go
@@ -1,11 +1,22 @@
 package userService
 
-import "hd-mall-ed/packages/client/models/userModel"
+import (
+	"github.com/thoas/go-funk"
+	"hd-mall-ed/packages/client/models/userModel"
+)
 
 func UserUpdate(user *userModel.User) error {
 	// save 是全量更新
 	updateMap := make(map[string]interface{})
-	updateMap["password"] = user.Password
+
+	// 更新密码
+	if !funk.IsEmpty(user.Password) {
+		updateMap["password"] = user.Password
+	}
+
+	if funk.IsEmpty(updateMap) {
+		return nil
+	}
 
 	return user.Update(updateMap)
 }
