47b8ba55c4634035ac8c98d174d299058dc900ff
yanlele <331393627@qq.com>
2021-02-05 21:06:05 +0800

    feat: 添加demo

---
+++ packages/demos/01_govalidator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asaskevich/govalidator"
+	"strings"
+)
+
+type addressStruct struct {
+	AddressName string
+	AddressCode string
+}
+
+type userStruct struct {
+	Name    string        `valid:"required~缺少姓名"`
+	Age     int           `valid:"required~缺少年龄"`
+	Address addressStruct `valid:"required~缺少地址信息"`
+}
+
+func main() {
+	user := userStruct{
+		Name: "yanle",
+		Age: 30,
+		Address: addressStruct{"12", ""},
+	}
+
+	result, err := govalidator.ValidateStruct(user)
+	fmt.Println("result: ", result)
+	if err != nil {
+		fmt.Println(strings.Split(err.Error(), ";")[0])
+	}
+}
