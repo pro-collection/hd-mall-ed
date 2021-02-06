ff979820bb535e1b9a9c0ee0109458afda882b1b
yanlele <331393627@qq.com>
2021-02-06 12:01:52 +0800

    feat: 添加auth check model 方法

--- packages/client/models/authModel/index.go
+++ packages/client/models/authModel/index.go
@@ -1,3 +1,26 @@
 package authModel
 
+import "hd-mall-ed/packages/client/database"
 
+type Auth struct {
+	Id       int    `json:"id" gorm:"primary_key"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func CheckAuth(username, password string) (int, error) {
+	var auth Auth
+	err := database.DataBase.Select("id").
+		Where(Auth{Username: username, Password: password}).
+		First(&auth).Error
+	if auth.Id > 0 {
+		return auth.Id, nil
+	}
+	return -1, err
+}
+
+func GetAuthById(id int64) Auth {
+	var auth Auth
+	database.DataBase.Where("id = ?", id).First(&auth)
+	return auth
+}
