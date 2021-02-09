5fbacca47f68a4800fc655f2220e1d842c7ab82a
yanlele <331393627@qq.com>
2021-02-09 23:35:05 +0800

    feat: 添加备注

--- packages/client/router/addressRouter.go
+++ packages/client/router/addressRouter.go
@@ -12,9 +12,11 @@ func addressRouter(router *gin.RouterGroup)  {
 	{
 		// 获取所有列表
 		address.GET("/list", addressController.GetAddressList)
-		// 创建列表
+		// 创建地址
 		address.POST("/create", addressController.Create)
 		// 通过 id 查询具体的地址
 		address.GET("/get", addressController.GetAddressById)
+		// 修改地址
+		// 删除地址
 	}
 }
