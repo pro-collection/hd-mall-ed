68ac559e76e0356e230e21d2ddd114686fdcb996
yanlele <331393627@qq.com>
2021-02-11 12:01:42 +0800

    feat: 调整用户信息的获取

--- packages/client/controller/userController/getUserInfo.go
+++ packages/client/controller/userController/getUserInfo.go
@@ -8,7 +8,6 @@ import (
 	"hd-mall-ed/packages/client/pkg/app"
 	"hd-mall-ed/packages/client/pkg/e"
 	"log"
-	"strconv"
 )
 
 // 查询用户信息， 需要登录 和 权限验证
@@ -17,17 +16,17 @@ func GetUserInfo(c *gin.Context) {
 	model := &userModel.User{}
 	api := app.ApiFunction{C: c}
 	baseUser := &tableModel.BaseUser{}
+	var err error
+	var user userModel.User
 
-	userId := c.DefaultQuery("id", "")
-	if userId == "" {
+	user, err = api.GetUser()
+	if err != nil {
 		api.ResFail(e.InvalidParams)
 		return
 	}
 
-	id, _ := strconv.Atoi(userId)
-
 	// 问题类型转换
-	user, err := model.FindUserById(uint(id))
+	user, err = model.FindUserById(user.ID)
 	if err != nil {
 		log.Println(err.Error())
 		api.ResFailMessage(e.Fail, err.Error())
