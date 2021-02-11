ff72a43eda121873234810310b29fcb0cf59fab5
yanlele <331393627@qq.com>
2021-02-11 23:48:21 +0800

    feat: 商品种类model

--- packages/client/models/categoryModel/index.go
+++ packages/client/models/categoryModel/index.go
@@ -1,3 +1,35 @@
 package categoryModel
 
+import (
+	"github.com/ulule/deepcopier"
+	"hd-mall-ed/packages/client/database"
+	"hd-mall-ed/packages/client/database/tableModel"
+)
 
+type Category tableModel.Category
+
+// 创建
+func (category *Category) Create() error {
+	return database.DataBase.Create(category).Error
+}
+
+// 查询
+func (category *Category) Get() ([]*tableModel.CategoryBase, error) {
+	var categoryList []*tableModel.CategoryBase
+	if err := database.DataBase.Model(&Category{}).Find(categoryList).Error; err != nil {
+		return categoryList, err
+	}
+	return categoryList, nil
+}
+
+// 更新
+func (category *Category) Update() error {
+	baseModel := tableModel.CategoryBase{}
+	_ = deepcopier.Copy(category).To(&baseModel)
+	return database.DataBase.Updates(baseModel).Error
+}
+
+// 删除
+func (category *Category) Delete() error {
+	return database.DataBase.Delete(category).Error
+}
