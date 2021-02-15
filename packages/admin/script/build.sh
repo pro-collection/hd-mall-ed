5b6a10d332495b2dc210980ab0baeb5e7387d576
yanlele <331393627@qq.com>
2021-02-15 22:12:00 +0800

    feat: 执行脚本

--- packages/admin/script/build.sh
+++ packages/admin/script/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./admin ./main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./admin ./../main.go
 
 scp ./admin yanle-tx:/root/app/hd_mell/admin
 scp ./run-admin.sh yanle-tx:/root/app/hd_mell/run-admin.sh
-scp ../common/config/appDev.ini yanle-tx:/root/app/hd_mell/packages/common/config/appDev.ini
+scp ../../common/config/appDev.ini yanle-tx:/root/app/hd_mell/packages/common/config/appDev.ini
