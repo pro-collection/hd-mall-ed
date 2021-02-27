948fb8e2f0de7e80d1a2f064421499431031df89
yanlele <331393627@qq.com>
2021-02-27 09:04:43 +0800

    feat: 修改分裂

--- packages/common/controller/categoryHelper/interface.go
+++ packages/common/controller/categoryHelper/interface.go
@@ -3,9 +3,12 @@ package categoryHelper
 import "hd-mall-ed/packages/common/database/tableModel"
 
 type ListItemResultStruct struct {
-	ID       uint                       `json:"id" gorm:"primarykey"`
-	Name     string                     `json:"name" gorm:"not null;comment:'商品分类'" valid:"required~缺少商品类别名称"`
-	Type     int                        `json:"type" gorm:"comment:'商品分类的类型'"`
-	Avatar   string                     `json:"avatar" gorm:"comment:'缩略图''"`
-	Children []*tableModel.CategoryBase `json:"children"`
+	ID              uint                       `json:"id" gorm:"primarykey"`
+	Name            string                     `json:"name" gorm:"not null;comment:'商品分类'" valid:"required~缺少商品类别名称"`
+	Type            int                        `json:"type" gorm:"comment:'商品分类的类型'"`
+	Avatar          string                     `json:"avatar" gorm:"comment:'缩略图''"`
+	BannerImage     string                     `json:"banner_image" gorm:"content:banner图片;type:longtext"`
+	BannerProductId uint                       `json:"banner_product_id" gorm:"content:banner对应的商品id, 可以实现跳转"`
+	BannerLink      string                     `json:"banner_link" gorm:"content:banner对应商品的跳转链接"`
+	Children        []*tableModel.CategoryBase `json:"children"`
 }
