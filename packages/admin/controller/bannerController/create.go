6b97ea5c6f5cd8af69a53225eec076b564f4906f
yanlele <331393627@qq.com>
2021-02-27 19:54:12 +0800

    feat: 创建

---
+++ packages/admin/controller/bannerController/create.go
@@ -0,0 +1,35 @@
+package bannerController
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thoas/go-funk"
+	"hd-mall-ed/packages/admin/models/staticModel"
+	"hd-mall-ed/packages/common/pkg/adminApp"
+	"hd-mall-ed/packages/common/pkg/e"
+)
+
+// 单个创建
+func Create(c *gin.Context) {
+	api := adminApp.ApiInit(c)
+	model := &staticModel.Static{}
+	err := c.ShouldBindJSON(model)
+
+	// 验证参数
+	if funk.IsEmpty(model) {
+		api.ResFail(e.InvalidParams)
+		return
+	}
+	if api.ValidateHasError(model) {
+		api.ResFail(e.InvalidParams)
+		return
+	}
+
+	// 创建入库
+	err = model.Create()
+	if err != nil {
+		api.ResFailMessage(e.Fail, err.Error())
+		return
+	}
+
+	api.ResponseNoData()
+}
