06f4a040f2340809d7ae21dcc4bc82341209388f
yanlele <331393627@qq.com>
2021-02-27 08:28:27 +0800

    feat: 修改商品种类表

--- packages/common/database/tableModel/category.go
+++ packages/common/database/tableModel/category.go
@@ -13,7 +13,9 @@ type CategoryBase struct {
 	Name     string `json:"name" gorm:"not null;comment:'商品分类'" valid:"required~缺少商品类别名称"`
 	Type     int    `json:"type" gorm:"comment:'商品分类的类型'"`
 	ParentId uint   `json:"parent_id" gorm:"index;comment:'父节点 id , 可以为空'"`
-	Avatar   string `json:"avatar" gorm:"comment:'缩略图''"`
+	Avatar   string `json:"avatar" gorm:"comment:缩略图"`
+	BannerImage string `json:"banner_image" gorm:"content:banner图片;type:longtext"`
+	BannerProductId uint `json:"banner_product_id" gorm:"content:banner对应的商品id, 可以实现跳转"`
 }
 
 type Category struct {
