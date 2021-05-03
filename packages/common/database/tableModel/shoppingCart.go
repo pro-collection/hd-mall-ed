1d08bd9df2e336ace10cbb7441cebc7bb2defcea
yanlele <331393627@qq.com>
2021-05-03 17:29:34 +0800

    feat: 重新更新表结构

--- packages/common/database/tableModel/shoppingCart.go
+++ packages/common/database/tableModel/shoppingCart.go
@@ -7,11 +7,17 @@ type 类型
 	3 - 购物车创建临时订单场景
 */
 type ShoppingCartBase struct {
-	ID        uint `json:"id" gorm:"primarykey"`
-	UserId    uint `json:"user_id" gorm:"index;comment:关联的UserId"`
-	ProductId uint `json:"product_id" gorm:"index;comment:关联的productId"`
-	Type      int  `json:"type" gorm:"not null;comment:类型" valid:"required"`
-	Count     int  `json:"count" gorm:"not null;comment:数量;default:1"`
+	ID           uint    `json:"id" gorm:"primarykey"`
+	UserId       uint    `json:"user_id" gorm:"index;comment:关联的UserId"`
+	ProductId    uint    `json:"product_id" gorm:"index;comment:关联的productId"`
+	TempOrderId  uint    `json:"temp_order_id" gorm:"comment:临时订单id"`
+	Type         int     `json:"type" gorm:"not null;comment:类型" valid:"required"`
+	Count        int     `json:"count" gorm:"not null;comment:数量;default:1"`
+	Name         string  `json:"name" gorm:"not null;comment:商品名称" valid:"required"`
+	Title        string  `json:"title" gorm:"not null;comment:title" valid:"required"`
+	Desc         string  `json:"desc" gorm:"comment:描述"`
+	Price        float32 `json:"price" gorm:"not null;comment:实际价格" valid:"required"`
+	PrimaryImage string  `json:"primary_image" gorm:"comment:主图;type:longtext" valid:"required"`
 }
 
 type ShoppingCart struct {
